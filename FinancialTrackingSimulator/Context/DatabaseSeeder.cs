54904e77127da45357d2fc8d2078aab64ca4bde3
Moboss2k05 <mobarizmirza.rizwan@gmail.com>
2024-10-14 10:34:01 +0100

    Started Seeder, Login and Registering

---
+++ FinancialTrackingSimulator/Context/DatabaseSeeder.cs
@@ -0,0 +1,12 @@
+﻿using FinancialTrackingSimulator.Model;
+
+namespace FinancialTrackingSimulator.Context
+{
+    public class DatabaseSeeder
+    {
+        public async Task Seed()
+        {
+
+        }
+    }
+}
